c1a842520734826a23190bcda7f36630ee01938d
hukasigi <hukasigiteya2@gmail.com>
2026-08-31 13:02:04 +0900

    途中で操作が効かなくなるが、とりあえず動いた

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ class Steering {
             this->target_degree = 0;
         }
         bool calibrate_zero() { // 0点合わせ
-            double startTime = millis();
+            uint32_t startTime = millis();
             this->motor->run(CALIBRATING_DUTY);
             if (this->limit_switch->active()) { // ONから始まったら、一度OFFになるまで待つ
                 this->motor->run(-CALIBRATING_DUTY);
@@ -43,12 +43,22 @@ class Steering {
         }
         void   set_target(double degree) { this->target_degree = degree; }
         double get_current_degree() {
-            return (encoder->getCount() * 360.0 / ENCODER_RESOLUTION) / STEER_GEAR_RATIO_MOTOR_TO_STEER;
+            double degree = (encoder->getCount() * 360.0 / ENCODER_RESOLUTION) / STEER_GEAR_RATIO_MOTOR_TO_STEER;
+
+            while (degree > 180.0)
+                degree -= 360.0;
+
+            while (degree < -180.0)
+                degree += 360.0;
+
+            return degree;
         }
         void update(double dt) {
             double current_degree = this->get_current_degree();
             double duty           = this->pid->update(this->target_degree, current_degree, dt);
-            this->motor->run(duty);
+            double error          = pid->getError();
+
+            this->motor->run(duty, -1);
         }
 
     private:
@@ -65,16 +75,12 @@ class Steering {
         static constexpr double STEER_GEAR_RATIO_MOTOR_TO_STEER = 65.0 / 27.0;
 };
 
-static double motor_rpmToMM_S(double motorRpm, double DRIVE_GEAR_RATIO, double DRIVE_RADIUS) {
-    const double wheelRpm = motorRpm / DRIVE_GEAR_RATIO;
-    return wheelRpm * 2.0 * M_PI * DRIVE_RADIUS / 60.0;
-}
-
 class Drive {
     public:
         Drive(RobomasMotor* motor, IncrementalPID* pid) {
-            this->motor = motor;
-            this->pid   = pid;
+            this->motor       = motor;
+            this->pid         = pid;
+            this->target_mm_s = 0.0;
         }
         void   set_target(double drive_target_mm_s) { this->target_mm_s = drive_target_mm_s; }
         double get_current_mm_s() { // rpm -> mm/s
@@ -115,10 +121,12 @@ class SwerveDrive {
             this->drive->update(dt);
         }
         void set_target(double degree, double drive_target_mm_s) {
-            double          current_degree = this->steering->get_current_degree();
-            OptimizedParams params         = optimizeSteerAngle(degree, current_degree);
+            double current_degree = this->steering->get_current_degree();
+
+            OptimizedParams params = optimizeSteerAngle(degree, current_degree);
+
             this->steering->set_target(params.degree);
-            this->drive->set_target(drive_target_mm_s);
+            this->drive->set_target(drive_target_mm_s * params.drive_dir);
         }
 
     private:
@@ -137,17 +145,38 @@ class SwerveDrive {
                 a += 360.0;
             return a;
         }
-        static OptimizedParams optimizeSteerAngle(double steer_target, double currentAngleDeg) {
-            const double    error = normalizeAngleDeg(steer_target - currentAngleDeg);
-            OptimizedParams result{steer_target, 1};
+        // static OptimizedParams optimizeSteerAngle(double steer_target, double currentAngleDeg) {
+        //     const double    error = normalizeAngleDeg(steer_target - currentAngleDeg);
+        //     OptimizedParams result{steer_target, 1};
 
+        //     if (error > 90.0) {
+        //         result.degree    = normalizeAngleDeg(steer_target - 180.0);
+        //         result.drive_dir = -1;
+        //     } else if (error < -90.0) {
+        //         result.degree    = normalizeAngleDeg(steer_target + 180.0);
+        //         result.drive_dir = -1;
+        //     }
+        //     return result;
+        // }
+        static OptimizedParams optimizeSteerAngle(double steer_target, double currentAngleDeg) {
+            double error = normalizeAngleDeg(steer_target - currentAngleDeg);
+
+            OptimizedParams result;
+            result.drive_dir = 1;
+
+            // 90°を超えるなら、車輪を180°反転して
+            // ドライブ方向を逆にする
             if (error > 90.0) {
-                result.degree    = normalizeAngleDeg(steer_target - 180.0);
+                error -= 180.0;
                 result.drive_dir = -1;
             } else if (error < -90.0) {
-                result.degree    = normalizeAngleDeg(steer_target + 180.0);
+                error += 180.0;
                 result.drive_dir = -1;
             }
+
+            // 現在角度の近くにある目標角度を作る
+            result.degree = currentAngleDeg + error;
+
             return result;
         }
 };
@@ -175,15 +204,15 @@ const int16_t STEER_MOTOR_POWER_LIMIT = 200.;
 const int16_t STEER_INTEGRAL_LIMIT    = 10.;
 const int16_t RANGE                   = 360;
 
-const int16_t DRIVE_MOTOR_POWER_LIMIT = 200.;
+const int16_t DRIVE_MOTOR_POWER_LIMIT = 50.;
 const int16_t DRIVE_INTEGRAL_LIMIT    = 10.;
 
 const double STICK_DEADZONE       = 15.0;
 const double DRIVE_MAX_SPEED_MM_S = 1000.0;
 
 const uint32_t        CONTROL_CYCLE_MS   = 10; // 10ms = 100Hz
-const struct PidParam STEERING_PID_PARAM = {.p_gain = 0.3, .i_gain = 0.0, .d_gain = 0.0};
-const struct PidParam DRIVE_PID_PARAM    = {.p_gain = 0.3, .i_gain = 0.0, .d_gain = 0.0};
+const struct PidParam STEERING_PID_PARAM = {.p_gain = 2.5, .i_gain = 0.0, .d_gain = 0.0};
+const struct PidParam DRIVE_PID_PARAM    = {.p_gain = 1.2, .i_gain = 0.0, .d_gain = 0.0};
 
 const uint32_t CONTROL_LOOP_TASK_STACK_SIZE = 8192;
 const uint8_t  CONTROL_LOOP_TASK_PRIORITY   = 10;
@@ -210,9 +239,24 @@ void control_loop_task(void* args) {
 
     while (true) {
         swerve_drive.update(CONTROL_CYCLE_MS / 1000.0);
+
+        // ドライブモータの指令値を CAN で送信
+        if (!can.send(&drive_motor)) {
+            // Serial.println("Drive CAN send failed");
+        }
         vTaskDelayUntil(&wake_time, pdMS_TO_TICKS(CONTROL_CYCLE_MS));
     }
 }
+// void control_loop_task(void* args) {
+//     while (true) {
+//         swerve_drive.update(0.01);
+//         if (!can.send(&drive_motor)) {
+//             Serial.println("Drive CAN send failed");
+//         }
+
+//         vTaskDelay(pdMS_TO_TICKS(10));
+//     }
+// }
 
 void setup() {
     Serial.begin(115200);
@@ -226,22 +270,42 @@ void setup() {
         while (true) {
         }
     }
-    PS4.begin("00:02:5b:00:a5:ac");
+    PS4.begin("08:d1:f9:37:41:f2");
     xTaskCreate(control_loop_task, "ControlLoopTask", CONTROL_LOOP_TASK_STACK_SIZE, NULL, CONTROL_LOOP_TASK_PRIORITY,
                 &control_loop_task_handle);
 }
 
 void loop() {
-    int    rx     = PS4.RStickX();
-    int    ry     = PS4.RStickY();
-    double degree = atan2((double)ry, (double)rx) * 180.0 / M_PI;
-    // スティック上を0度にするために90度シフト
-    degree          = degree - 90.0;
+
+    if (!PS4.isConnected()) {
+        Serial.println("PS4 not connected");
+        delay(100);
+        return;
+    }
+
+    int rx = PS4.RStickX();
+    int ry = PS4.RStickY();
+
     double stickMag = hypot((double)rx, (double)ry);
+
+    if (stickMag <= STICK_DEADZONE) {
+        swerve_drive.set_target(steering.get_current_degree(), 0.0);
+
+        delay(10);
+        return;
+    }
+
+    double degree = atan2((double)ry, (double)rx) * 180.0 / M_PI;
 
     double maxStickMag = 127.0 * sqrt(2.0);
 
     double drive_target_mm_s = (stickMag - STICK_DEADZONE) / (maxStickMag - STICK_DEADZONE) * DRIVE_MAX_SPEED_MM_S;
+
+    drive_target_mm_s = constrain(drive_target_mm_s, 0.0, DRIVE_MAX_SPEED_MM_S);
+
+    static uint32_t last_log_ms = 0;
+    uint32_t        now         = millis();
     swerve_drive.set_target(degree, drive_target_mm_s);
-    delay(10); // 適当
+
+    delay(10);
 }
