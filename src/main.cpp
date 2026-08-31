0ef0af5b7950522c44832e4adf867b8f11960a96
hukasigi <hukasigiteya2@gmail.com>
2026-08-31 14:38:12 +0900

    原点取りできた

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,13 @@ using namespace nnct::interfaces;
 
 class Steering {
     public:
-        Steering(Motor* motor, IncrementalEncoder* encoder, LimitSwitch* limit_switch, AnglePID* pid) {
+        Steering(Motor* motor, IncrementalEncoder* encoder, LimitSwitch* limit_switch, AnglePID* pid, double offset_degree) {
             this->motor         = motor;
             this->encoder       = encoder;
             this->limit_switch  = limit_switch;
             this->pid           = pid;
             this->target_degree = 0;
+            this->offset_degree = offset_degree;
         }
         bool calibrate_zero() { // 0点合わせ
             uint32_t startTime = millis();
@@ -43,15 +44,12 @@ class Steering {
         }
         void   set_target(double degree) { this->target_degree = degree; }
         double get_current_degree() {
-            double degree = (encoder->getCount() * 360.0 / ENCODER_RESOLUTION) / STEER_GEAR_RATIO_MOTOR_TO_STEER;
+            double degree = (this->encoder->getCount() * 360.0 / ENCODER_RESOLUTION) / STEER_GEAR_RATIO_MOTOR_TO_STEER;
 
-            while (degree > 180.0)
-                degree -= 360.0;
+            // offset を角度計算時に補正
+            degree -= this->offset_degree;
 
-            while (degree < -180.0)
-                degree += 360.0;
-
-            return degree;
+            return normalizeAngleDeg(degree);
         }
         void update(double dt) {
             double current_degree = this->get_current_degree();
@@ -62,12 +60,20 @@ class Steering {
         }
 
     private:
+        static double normalizeAngleDeg(double a) {
+            while (a > 180.0)
+                a -= 360.0;
+            while (a < -180.0)
+                a += 360.0;
+            return a;
+        }
         Motor*              motor;
         IncrementalEncoder* encoder;
         LimitSwitch*        limit_switch;
         AnglePID*           pid;
 
         double target_degree;
+        double offset_degree = 0.;
 
         static const int32_t    CALIBRATING_DUTY                = 150;
         static const uint32_t   CALIBRATING_TIMEOUT_MS          = 8000;
@@ -110,6 +116,15 @@ class SwerveDrive {
             this->drive    = drive;
             this->steering = steering;
         }
+
+        void set_target(double degree, double drive_target_mm_s) {
+            double current_degree = this->steering->get_current_degree();
+
+            OptimizedParams params = optimizeSteerAngle(degree, current_degree);
+
+            this->steering->set_target(params.degree);
+            this->drive->set_target(drive_target_mm_s * params.drive_dir);
+        }
         bool init() {
             if (!this->steering->calibrate_zero()) {
                 return false;
@@ -119,14 +134,6 @@ class SwerveDrive {
         void update(double dt) {
             this->steering->update(dt);
             this->drive->update(dt);
-        }
-        void set_target(double degree, double drive_target_mm_s) {
-            double current_degree = this->steering->get_current_degree();
-
-            OptimizedParams params = optimizeSteerAngle(degree, current_degree);
-
-            this->steering->set_target(params.degree);
-            this->drive->set_target(drive_target_mm_s * params.drive_dir);
         }
 
     private:
@@ -145,19 +152,6 @@ class SwerveDrive {
                 a += 360.0;
             return a;
         }
-        // static OptimizedParams optimizeSteerAngle(double steer_target, double currentAngleDeg) {
-        //     const double    error = normalizeAngleDeg(steer_target - currentAngleDeg);
-        //     OptimizedParams result{steer_target, 1};
-
-        //     if (error > 90.0) {
-        //         result.degree    = normalizeAngleDeg(steer_target - 180.0);
-        //         result.drive_dir = -1;
-        //     } else if (error < -90.0) {
-        //         result.degree    = normalizeAngleDeg(steer_target + 180.0);
-        //         result.drive_dir = -1;
-        //     }
-        //     return result;
-        // }
         static OptimizedParams optimizeSteerAngle(double steer_target, double currentAngleDeg) {
             double error = normalizeAngleDeg(steer_target - currentAngleDeg);
 
@@ -190,15 +184,36 @@ struct PidParam {
 using pin_t = uint8_t;
 using ch_t  = uint8_t;
 
-const pin_t STEERING_MOTOR_DIR = 23;
-const pin_t STEERING_MOTOR_PWM = 22;
-const ch_t  STEERING_MOTOR_CH  = 0;
-const id_t  DRIVE_MOTOR_ID     = 0x01;
-const pin_t STEERING_ENCODER_A = 27;
-const pin_t STEERING_ENCODER_B = 14;
-const pin_t STEERING_LIMIT_SW  = 36;
-const pin_t CAN_RX_PIN         = 4; // 実際の配線に合わせて変更
-const pin_t CAN_TX_PIN         = 5; // 実際の配線に合わせて変更
+const pin_t STEERING_MOTOR_DIR_1 = 23;
+const pin_t STEERING_MOTOR_PWM_1 = 22;
+const ch_t  STEERING_MOTOR_CH_1  = 0;
+const id_t  DRIVE_MOTOR_ID_1     = 0x01;
+const pin_t STEERING_ENCODER_A_1 = 27;
+const pin_t STEERING_ENCODER_B_1 = 14;
+const pin_t STEERING_LIMIT_SW_1  = 36;
+
+const pin_t STEERING_MOTOR_DIR_2 = 21;
+const pin_t STEERING_MOTOR_PWM_2 = 19;
+const ch_t  STEERING_MOTOR_CH_2  = 1;
+const id_t  DRIVE_MOTOR_ID_2     = 0x02;
+const pin_t STEERING_ENCODER_A_2 = 25;
+const pin_t STEERING_ENCODER_B_2 = 26;
+const pin_t STEERING_LIMIT_SW_2  = 39;
+
+const pin_t STEERING_MOTOR_DIR_3 = 18;
+const pin_t STEERING_MOTOR_PWM_3 = 17;
+const ch_t  STEERING_MOTOR_CH_3  = 2;
+const id_t  DRIVE_MOTOR_ID_3     = 0x04;
+const pin_t STEERING_ENCODER_A_3 = 32;
+const pin_t STEERING_ENCODER_B_3 = 33;
+const pin_t STEERING_LIMIT_SW_3  = 34;
+
+const double OFFSET_DEG_1 = 135;
+const double OFFSET_DEG_2 = 225;
+const double OFFSET_DEG_3 = 345;
+
+const pin_t CAN_RX_PIN = 4; // 実際の配線に合わせて変更
+const pin_t CAN_TX_PIN = 5; // 実際の配線に合わせて変更
 
 const int16_t STEER_MOTOR_POWER_LIMIT = 200.;
 const int16_t STEER_INTEGRAL_LIMIT    = 10.;
@@ -219,44 +234,63 @@ const uint8_t  CONTROL_LOOP_TASK_PRIORITY   = 10;
 
 TaskHandle_t control_loop_task_handle;
 
-Motor              steering_motor(STEERING_MOTOR_DIR, STEERING_MOTOR_PWM, STEERING_MOTOR_CH);
-IncrementalEncoder steering_encoder(STEERING_ENCODER_A, STEERING_ENCODER_B);
-LimitSwitch        steering_limit_switch(STEERING_LIMIT_SW);
-AnglePID steering_pid(STEERING_PID_PARAM.p_gain, STEERING_PID_PARAM.i_gain, STEERING_PID_PARAM.d_gain, -STEER_MOTOR_POWER_LIMIT,
-                      STEER_MOTOR_POWER_LIMIT, -STEER_INTEGRAL_LIMIT, STEER_INTEGRAL_LIMIT, RANGE);
-Steering steering(&steering_motor, &steering_encoder, &steering_limit_switch, &steering_pid);
+Motor              steering_motor_1(STEERING_MOTOR_DIR_1, STEERING_MOTOR_PWM_1, STEERING_MOTOR_CH_1);
+IncrementalEncoder steering_encoder_1(STEERING_ENCODER_A_1, STEERING_ENCODER_B_1);
+LimitSwitch        steering_limit_switch_1(STEERING_LIMIT_SW_1);
+AnglePID           steering_pid_1(STEERING_PID_PARAM.p_gain, STEERING_PID_PARAM.i_gain, STEERING_PID_PARAM.d_gain,
+                                  -STEER_MOTOR_POWER_LIMIT, STEER_MOTOR_POWER_LIMIT, -STEER_INTEGRAL_LIMIT, STEER_INTEGRAL_LIMIT, RANGE);
+Steering           steering_1(&steering_motor_1, &steering_encoder_1, &steering_limit_switch_1, &steering_pid_1, OFFSET_DEG_1);
 
-RobomasMotor   drive_motor(DRIVE_MOTOR_ID);
-RobomasCAN     can(CAN_RX_PIN, CAN_TX_PIN);
+Motor              steering_motor_2(STEERING_MOTOR_DIR_2, STEERING_MOTOR_PWM_2, STEERING_MOTOR_CH_2);
+IncrementalEncoder steering_encoder_2(STEERING_ENCODER_A_2, STEERING_ENCODER_B_2);
+LimitSwitch        steering_limit_switch_2(STEERING_LIMIT_SW_2);
+AnglePID           steering_pid_2(STEERING_PID_PARAM.p_gain, STEERING_PID_PARAM.i_gain, STEERING_PID_PARAM.d_gain,
+                                  -STEER_MOTOR_POWER_LIMIT, STEER_MOTOR_POWER_LIMIT, -STEER_INTEGRAL_LIMIT, STEER_INTEGRAL_LIMIT, RANGE);
+Steering           steering_2(&steering_motor_2, &steering_encoder_2, &steering_limit_switch_2, &steering_pid_2, OFFSET_DEG_2);
+
+Motor              steering_motor_3(STEERING_MOTOR_DIR_3, STEERING_MOTOR_PWM_3, STEERING_MOTOR_CH_3);
+IncrementalEncoder steering_encoder_3(STEERING_ENCODER_A_3, STEERING_ENCODER_B_3);
+LimitSwitch        steering_limit_switch_3(STEERING_LIMIT_SW_3);
+AnglePID           steering_pid_3(STEERING_PID_PARAM.p_gain, STEERING_PID_PARAM.i_gain, STEERING_PID_PARAM.d_gain,
+                                  -STEER_MOTOR_POWER_LIMIT, STEER_MOTOR_POWER_LIMIT, -STEER_INTEGRAL_LIMIT, STEER_INTEGRAL_LIMIT, RANGE);
+Steering           steering_3(&steering_motor_3, &steering_encoder_3, &steering_limit_switch_3, &steering_pid_3, OFFSET_DEG_3);
+
+RobomasMotor drive_motor_1(DRIVE_MOTOR_ID_1);
+RobomasMotor drive_motor_2(DRIVE_MOTOR_ID_2);
+RobomasMotor drive_motor_3(DRIVE_MOTOR_ID_3);
+
+RobomasCAN can(CAN_RX_PIN, CAN_TX_PIN);
+
 IncrementalPID drive_pid(DRIVE_PID_PARAM.p_gain, DRIVE_PID_PARAM.i_gain, DRIVE_PID_PARAM.d_gain, -DRIVE_MOTOR_POWER_LIMIT,
                          DRIVE_MOTOR_POWER_LIMIT, -DRIVE_INTEGRAL_LIMIT, DRIVE_INTEGRAL_LIMIT);
-Drive          drive(&drive_motor, &drive_pid);
 
-SwerveDrive swerve_drive(&drive, &steering);
+Drive drive_1(&drive_motor_1, &drive_pid);
+
+Drive drive_2(&drive_motor_2, &drive_pid);
+
+Drive drive_3(&drive_motor_3, &drive_pid);
+
+SwerveDrive swerve_drive_1(&drive_1, &steering_1);
+SwerveDrive swerve_drive_2(&drive_2, &steering_2);
+SwerveDrive swerve_drive_3(&drive_3, &steering_3);
 
 void control_loop_task(void* args) {
     TickType_t wake_time = xTaskGetTickCount();
 
     while (true) {
-        swerve_drive.update(CONTROL_CYCLE_MS / 1000.0);
+        // swerve_drive_1.update(CONTROL_CYCLE_MS / 1000.0);
+        // swerve_drive_2.update(CONTROL_CYCLE_MS / 1000.0);
+        // swerve_drive_3.update(CONTROL_CYCLE_MS / 1000.0);
 
         // ドライブモータの指令値を CAN で送信
-        if (!can.send(&drive_motor)) {
-            // Serial.println("Drive CAN send failed");
-        }
-        vTaskDelayUntil(&wake_time, pdMS_TO_TICKS(CONTROL_CYCLE_MS));
-    }
-}
-// void control_loop_task(void* args) {
-//     while (true) {
-//         swerve_drive.update(0.01);
-//         if (!can.send(&drive_motor)) {
-//             Serial.println("Drive CAN send failed");
-//         }
+        // if (!can.send(&drive_motor_1, &drive_motor_2, 0, &drive_motor_3)) {
 
-//         vTaskDelay(pdMS_TO_TICKS(10));
-//     }
-// }
+        //     // Serial.println("Drive CAN send failed");
+        // }
+    }
+
+    vTaskDelayUntil(&wake_time, pdMS_TO_TICKS(CONTROL_CYCLE_MS));
+}
 
 void setup() {
     Serial.begin(115200);
@@ -265,8 +299,18 @@ void setup() {
         while (true) {
         }
     }
-    if (!swerve_drive.init()) {
-        Serial.println("SwerveDrive init failed");
+    if (!swerve_drive_1.init()) {
+        Serial.println("SwerveDrive_1 init failed");
+        while (true) {
+        }
+    }
+    if (!swerve_drive_2.init()) {
+        Serial.println("SwerveDrive_2 init failed");
+        while (true) {
+        }
+    }
+    if (!swerve_drive_3.init()) {
+        Serial.println("SwerveDrive_3 init failed");
         while (true) {
         }
     }
@@ -289,8 +333,10 @@ void loop() {
     double stickMag = hypot((double)rx, (double)ry);
 
     if (stickMag <= STICK_DEADZONE) {
-        swerve_drive.set_target(steering.get_current_degree(), 0.0);
-
+        swerve_drive_1.set_target(steering_1.get_current_degree(), 0.0);
+        swerve_drive_2.set_target(steering_1.get_current_degree(), 0.0);
+        swerve_drive_3.set_target(steering_1.get_current_degree(), 0.0);
+        drive_pid.reset();
         delay(10);
         return;
     }
@@ -305,7 +351,13 @@ void loop() {
 
     static uint32_t last_log_ms = 0;
     uint32_t        now         = millis();
-    swerve_drive.set_target(degree, drive_target_mm_s);
+    swerve_drive_1.set_target(degree, drive_target_mm_s);
+    swerve_drive_2.set_target(degree, drive_target_mm_s);
+    swerve_drive_3.set_target(degree, drive_target_mm_s);
+
+    swerve_drive_1.update(CONTROL_CYCLE_MS / 1000.0);
+    swerve_drive_2.update(CONTROL_CYCLE_MS / 1000.0);
+    swerve_drive_3.update(CONTROL_CYCLE_MS / 1000.0);
 
     delay(10);
 }
