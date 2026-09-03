86167d51c1714ea5818a2613f4be5e24ccc41784
hukasigi <hukasigiteya2@gmail.com>
2026-09-03 12:43:51 +0900

    コードをきれいに整形下

--- src/main.cpp
+++ src/main.cpp
@@ -190,55 +190,76 @@ struct PidParam {
 using pin_t = uint8_t;
 using ch_t  = uint8_t;
 
-const pin_t STEERING_MOTOR_DIR_1 = 23;
-const pin_t STEERING_MOTOR_PWM_1 = 22;
-const ch_t  STEERING_MOTOR_CH_1  = 0;
-const id_t  DRIVE_MOTOR_ID_1     = 0x01;
-const pin_t STEERING_ENCODER_A_1 = 27;
-const pin_t STEERING_ENCODER_B_1 = 14;
-const pin_t STEERING_LIMIT_SW_1  = 36;
+// ステアリング1
+const pin_t  STEERING_MOTOR_DIR_1 = 23;
+const pin_t  STEERING_MOTOR_PWM_1 = 22;
+const ch_t   STEERING_MOTOR_CH_1  = 0;
+const id_t   DRIVE_MOTOR_ID_1     = 0x01;
+const pin_t  STEERING_ENCODER_A_1 = 27;
+const pin_t  STEERING_ENCODER_B_1 = 14;
+const pin_t  STEERING_LIMIT_SW_1  = 36;
+const double OFFSET_DEG_1         = 45.;
 
-const pin_t STEERING_MOTOR_DIR_2 = 21;
-const pin_t STEERING_MOTOR_PWM_2 = 19;
-const ch_t  STEERING_MOTOR_CH_2  = 1;
-const id_t  DRIVE_MOTOR_ID_2     = 0x02;
-const pin_t STEERING_ENCODER_A_2 = 25;
-const pin_t STEERING_ENCODER_B_2 = 26;
-const pin_t STEERING_LIMIT_SW_2  = 39;
+// ステアリング2
+const pin_t  STEERING_MOTOR_DIR_2 = 21;
+const pin_t  STEERING_MOTOR_PWM_2 = 19;
+const ch_t   STEERING_MOTOR_CH_2  = 1;
+const id_t   DRIVE_MOTOR_ID_2     = 0x02;
+const pin_t  STEERING_ENCODER_A_2 = 25;
+const pin_t  STEERING_ENCODER_B_2 = 26;
+const pin_t  STEERING_LIMIT_SW_2  = 39;
+const double OFFSET_DEG_2         = 165.;
 
-const pin_t STEERING_MOTOR_DIR_3 = 18;
-const pin_t STEERING_MOTOR_PWM_3 = 17;
-const ch_t  STEERING_MOTOR_CH_3  = 2;
-const id_t  DRIVE_MOTOR_ID_3     = 0x04;
-const pin_t STEERING_ENCODER_A_3 = 32;
-const pin_t STEERING_ENCODER_B_3 = 33;
-const pin_t STEERING_LIMIT_SW_3  = 34;
-
-const double OFFSET_DEG_1 = 45.;
-const double OFFSET_DEG_2 = 165.;
-const double OFFSET_DEG_3 = 285.;
+// ステアリング3
+const pin_t  STEERING_MOTOR_DIR_3 = 18;
+const pin_t  STEERING_MOTOR_PWM_3 = 17;
+const ch_t   STEERING_MOTOR_CH_3  = 2;
+const id_t   DRIVE_MOTOR_ID_3     = 0x04;
+const pin_t  STEERING_ENCODER_A_3 = 32;
+const pin_t  STEERING_ENCODER_B_3 = 33;
+const pin_t  STEERING_LIMIT_SW_3  = 34;
+const double OFFSET_DEG_3         = 285.;
 
 const pin_t CAN_RX_PIN = 4; // 実際の配線に合わせて変更
 const pin_t CAN_TX_PIN = 5; // 実際の配線に合わせて変更
 
+// ステア制御パラメータ
 const int16_t STEER_MOTOR_POWER_LIMIT = 200;
 const int16_t STEER_INTEGRAL_LIMIT    = 10;
 const int16_t RANGE                   = 360;
 
+// ドライブ制御パラメータ
 const int16_t DRIVE_MOTOR_POWER_LIMIT = 255.;
 const int16_t DRIVE_INTEGRAL_LIMIT    = 10.;
 
-const double STICK_DEADZONE       = 15.0;
-const double DRIVE_MAX_SPEED_MM_S = 1000.0;
+// コントローラ
+const double     STICK_DEADZONE       = 15.0;
+const double     DRIVE_MAX_SPEED_MM_S = 1000.0;
+const uint32_t   CONTROL_CYCLE_MS     = 10; // 10ms = 100Hz
+constexpr double CONTROL_CYCLE_S      = CONTROL_CYCLE_MS / 1000.0;
 
-const uint32_t        CONTROL_CYCLE_MS   = 10; // 10ms = 100Hz
+// PIDパラメータ
 const struct PidParam STEERING_PID_PARAM = {.p_gain = 4.4, .i_gain = 0.2, .d_gain = 0.0};
 const struct PidParam DRIVE_PID_PARAM    = {.p_gain = 1.2, .i_gain = 0.0, .d_gain = 0.0};
 
-const uint32_t CONTROL_LOOP_TASK_STACK_SIZE = 8192;
-const uint8_t  CONTROL_LOOP_TASK_PRIORITY   = 10;
+// FreeRTOS
+constexpr uint32_t CONTROL_LOOP_TASK_STACK_SIZE = 8192;
+constexpr uint8_t  CONTROL_LOOP_TASK_PRIORITY   = 10;
+constexpr uint32_t LOOP_DELAY_MS                = 10;
 
 TaskHandle_t control_loop_task_handle;
+
+// ステアリング・ドライブシステムの配列
+struct SwerveModule {
+        Motor*              steering_motor;
+        IncrementalEncoder* steering_encoder;
+        LimitSwitch*        steering_limit_switch;
+        AnglePID*           steering_pid;
+        Steering*           steering;
+        RobomasMotor*       drive_motor;
+        Drive*              drive;
+        SwerveDrive*        swerve_drive;
+};
 
 Motor              steering_motor_1(STEERING_MOTOR_DIR_1, STEERING_MOTOR_PWM_1, STEERING_MOTOR_CH_1);
 IncrementalEncoder steering_encoder_1(STEERING_ENCODER_A_1, STEERING_ENCODER_B_1);
@@ -271,28 +292,64 @@ IncrementalPID drive_pid(DRIVE_PID_PARAM.p_gain, DRIVE_PID_PARAM.i_gain, DRIVE_P
                          DRIVE_MOTOR_POWER_LIMIT, -DRIVE_INTEGRAL_LIMIT, DRIVE_INTEGRAL_LIMIT);
 
 Drive drive_1(&drive_motor_1, &drive_pid);
-
 Drive drive_2(&drive_motor_2, &drive_pid);
-
 Drive drive_3(&drive_motor_3, &drive_pid);
 
 SwerveDrive swerve_drive_1(&drive_1, &steering_1);
 SwerveDrive swerve_drive_2(&drive_2, &steering_2);
 SwerveDrive swerve_drive_3(&drive_3, &steering_3);
 
+SwerveDrive*     swerve_drives[]    = {&swerve_drive_1, &swerve_drive_2, &swerve_drive_3};
+constexpr size_t NUM_SWERVE_MODULES = 3;
+
 void control_loop_task(void* args) {
     TickType_t wake_time = xTaskGetTickCount();
 
     while (true) {
-        swerve_drive_1.update(CONTROL_CYCLE_MS / 1000.0);
-        swerve_drive_2.update(CONTROL_CYCLE_MS / 1000.0);
-        swerve_drive_3.update(CONTROL_CYCLE_MS / 1000.0);
+        for (size_t i = 0; i < NUM_SWERVE_MODULES; i++) {
+            swerve_drives[i]->update(CONTROL_CYCLE_S);
+        }
 
         // ドライブモータの指令値を CAN で送信
         if (!can.send(&drive_motor_1, &drive_motor_2, 0, &drive_motor_3)) {
             Serial.println("Drive CAN send failed");
         }
         vTaskDelayUntil(&wake_time, pdMS_TO_TICKS(CONTROL_CYCLE_MS));
+    }
+}
+
+bool initialize_swerve_drives() {
+    for (size_t i = 0; i < NUM_SWERVE_MODULES; i++) {
+        if (!swerve_drives[i]->init()) {
+            Serial.printf("SwerveDrive_%d init failed\n", i + 1);
+            return false;
+        }
+    }
+    return true;
+}
+
+void handle_controller_input(int x_vec, int y_vec, uint8_t drive_power) {
+    double stickMag = hypot((double)x_vec, (double)y_vec);
+
+    if (stickMag <= STICK_DEADZONE) {
+        for (size_t i = 0; i < NUM_SWERVE_MODULES; i++) {
+            swerve_drives[i]->set_target(0.0, 0.0);
+        }
+        drive_pid.reset();
+        return;
+    }
+
+    double degree            = atan2((double)y_vec, (double)x_vec) * 180.0 / M_PI;
+    double drive_target_duty = drive_power;
+
+    for (size_t i = 0; i < NUM_SWERVE_MODULES; i++) {
+        swerve_drives[i]->set_target(degree, drive_target_duty);
+    }
+}
+
+void update_swerve_drives() {
+    for (size_t i = 0; i < NUM_SWERVE_MODULES; i++) {
+        swerve_drives[i]->update(CONTROL_CYCLE_S);
     }
 }
 
@@ -303,18 +360,7 @@ void setup() {
         while (true) {
         }
     }
-    if (!swerve_drive_1.init()) {
-        Serial.println("SwerveDrive_1 init failed");
-        while (true) {
-        }
-    }
-    if (!swerve_drive_2.init()) {
-        Serial.println("SwerveDrive_2 init failed");
-        while (true) {
-        }
-    }
-    if (!swerve_drive_3.init()) {
-        Serial.println("SwerveDrive_3 init failed");
+    if (!initialize_swerve_drives()) {
         while (true) {
         }
     }
@@ -324,7 +370,6 @@ void setup() {
 }
 
 void loop() {
-
     if (!PS4.isConnected()) {
         Serial.println("PS4 not connected");
         delay(100);
@@ -335,34 +380,8 @@ void loop() {
     int     ry     = PS4.RStickY();
     uint8_t r2_val = PS4.R2Value();
 
-    double stickMag = hypot((double)rx, (double)ry);
+    handle_controller_input(rx, ry, r2_val);
+    update_swerve_drives();
 
-    if (stickMag <= STICK_DEADZONE) {
-        swerve_drive_1.set_target(steering_1.get_current_degree(), 0.0);
-        swerve_drive_2.set_target(steering_2.get_current_degree(), 0.0);
-        swerve_drive_3.set_target(steering_3.get_current_degree(), 0.0);
-        drive_pid.reset();
-        delay(10);
-        return;
-    }
-
-    double degree = atan2((double)ry, (double)rx) * 180.0 / M_PI;
-
-    double maxStickMag = 127.0 * sqrt(2.0);
-
-    // double drive_target_duty = stickMag / maxStickMag * 100;
-
-    double drive_target_duty = r2_val;
-
-    static uint32_t last_log_ms = 0;
-    uint32_t        now         = millis();
-    swerve_drive_1.set_target(degree, drive_target_duty);
-    swerve_drive_2.set_target(degree, drive_target_duty);
-    swerve_drive_3.set_target(degree, drive_target_duty);
-
-    swerve_drive_1.update(CONTROL_CYCLE_MS / 1000.0);
-    swerve_drive_2.update(CONTROL_CYCLE_MS / 1000.0);
-    swerve_drive_3.update(CONTROL_CYCLE_MS / 1000.0);
-
-    delay(10);
+    delay(LOOP_DELAY_MS);
 }
